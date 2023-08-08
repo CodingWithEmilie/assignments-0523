f0bd088a184f35872aeb89f58f9e074cdd14e1ec
Emilie <emiliewu@163.com>
2023-08-08 13:31:30 -0700

    update

--- history/W2D2/exercise.js
+++
@@ -1,6 +0,0 @@
-// We have 3 files
-// each of them have a number written in the file
-// read each file, get the value from each file
-// subtract data2 and data3 numbers from data1 number
-
-const fs = require("fs");
