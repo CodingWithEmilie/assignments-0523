8cea282e9f7e120c8838b15f5da98ffbef1cc489
Emilie <emiliewu@163.com>
2023-08-09 11:12:38 -0700

    update

---
+++ JQuery/day1/todo.js
@@ -0,0 +1,25 @@
+
+  //Get items list by id
+  
+  //Create a function addTaskToList to append task to the list
+  // create li for each task
+  // add classes for styling
+  // append to ul each task
+  
+  // Load tasks using loop and calling function addTaskToList
+  
+  //Get submit button by id
+  
+  // Add event listener on submit button to add new tasks by clicking it
+  
+  // fetch data from jsonplaceholder
+  // https://jsonplaceholder.typicode.com/todos
+  
+  /*
+  - Add event listener to task list
+  - Check if icon delete or icon edit was clicked
+  - If icon delete clicked, remove parent element
+  - If icon edit clicked, hide span, show input and add span text to input
+  - For edit icon, add event listener to press enter key, to update and show span text and hide input
+  */
+  
